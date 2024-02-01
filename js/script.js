a71882b0d5fdf42be34e411fd54fa88511b37103
muntasir ahmed shafin <155794937+muntasir-shafin@users.noreply.github.com>
2024-02-01 14:13:01 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$('.main_banner_slider').slick({
+    arrows:false,
+    dots:true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    speed: 1000,
+    fade: true
+
+});
+
+$('.main_slider').slick({
+    arrows:true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow:`<i class="fa-solid fa-angle-left left"></i>`,
+    nextArrow:`<i class="fa-solid fa-angle-right right"></i>`,
+    centerMode: true,
+    centerPadding: '0',
+    responsive: [
+        
+        {
+          breakpoint: 575.98,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+});
